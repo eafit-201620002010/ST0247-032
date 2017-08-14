f2393a6523d6cbd17bbc4dad2bbc3b1bf645ec33
Mateo <marami26@eafit.edu.co>
2017-08-14 18:46:47 -0500

    Añadiendo dijsktra

--- laboratorios/lab01/codigo/GraphAlgorithms.py
+++ laboratorios/lab01/codigo/GraphAlgorithms.py
@@ -1,5 +1,6 @@
 import DigraphAM
 import DigraphAL
+from collections import defaultdict
 """
     @archivo GraphAlgorithms
     Descripcion: implementacion algoritmos para probar en grafos.
@@ -42,3 +43,41 @@ def punto1_2(grafo):
 			maximo=1
 	#retorno el maximo
 	return maximo
+
+#Adaptado de: https://gist.github.com/econchick/4666413
+def dijsktra(graph, initial):
+  visited = {initial: 0}
+  path = {}
+  nodes = set()
+
+  for i in range(graph.size+1):
+  	nodes.add(i)
+
+  while nodes: 
+    min_node = None
+    for node in nodes:
+      if node in visited:
+        if min_node is None:
+          min_node = node
+        elif visited[node] < visited[min_node]:
+          min_node = node
+
+    if min_node is None:
+      break
+
+    nodes.remove(min_node)
+    current_weight = visited[min_node]
+
+    edges=defaultdict(list)
+    for i in range(graph.size+1):
+    	successors = graph.get_successors(i)
+    	for k in range(len(successors)):
+    		edges[i].append(successors[k])
+
+    for edge in edges[min_node]:
+      weight = current_weight + graph.get_weight(min_node, edge)
+      if edge not in visited or weight < visited[edge]:
+        visited[edge] = weight
+        path[edge] = min_node
+
+  return visited, path
