e4b437568b68d49afea24c42e482f0818e6c6fdc
Mateo <marami26@eafit.edu.co>
2017-08-26 10:01:30 -0500

    Añadiendo punto 3

--- talleres/taller02/Entrega/taller02.py
+++ talleres/taller02/Entrega/taller02.py
@@ -1,6 +1,5 @@
 #Fuerza Bruta
 
-
 # 1
 def punto1(start, nums, target):
     if start==len(nums):
@@ -15,3 +14,18 @@ def punto2(pre, pos, resultado):
 		return resultado
 	return punto2(pre+pos[:1],pos[1:],resultado) and punto2(pre,pos[1:],resultado)
 
+# 3
+def punto3(pre, pos, resultado):
+	if len(pos)==0:
+		resultado.add(pre)
+	for i in range(len(pos)):
+		punto3(pre+pos[i],pos[:i]+pos[i+1:],resultado)
+	return resultado
+
+# 4
+#def esValido([]):
+
+#def queens(n):
+
+
+	
