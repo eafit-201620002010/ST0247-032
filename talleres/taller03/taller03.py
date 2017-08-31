56f71b08929e7b01b217ba152a203739c10908e3
Mateo <marami26@eafit.edu.co>
2017-08-30 21:58:37 -0500

    Taller 3 punto 1

--- talleres/taller03/taller03.py
+++ talleres/taller03/taller03.py
@@ -1,3 +1,23 @@
-def puedo_poner_reina(r,c,tablero):
-	if abs(int(tablero[i])-int(tablero[k]))==k-i or tablero[i]==tablero[k]:
-		return False
+
+def puedo_poner_reina(r,tablero):
+	for i in range(r):
+		if abs(tablero[i]-tablero[r])==r-i or tablero[i]==tablero[r]:
+			return False
+	return True
+
+
+def n_reinas(n):
+	tablero = [None]*n
+	return len(n_reinas_aux([],0,tablero)) 
+
+
+
+def n_reinas_aux(r,n,tablero):
+	if n>=len(tablero):
+		r.append(tablero)
+	else:
+		for i in range(len(tablero)):
+			tablero[n]=i
+			if puedo_poner_reina(n,tablero):
+				n_reinas_aux(r,n+1,tablero)
+	return r
