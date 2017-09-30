dc1d412c4d9f139e60d5b291e3da8b9a71a27f7d
Mateo <marami26@eafit.edu.co>
2017-09-30 13:44:56 -0500

    documentacion hmtl

--- laboratorios/lab02/codigo/Ejercicio2.py
+++ laboratorios/lab02/codigo/Ejercicio2.py
@@ -1,10 +1,12 @@
-def puedo_poner_reina(r,tablero):
-	for i in range(r):
-		if abs(tablero[i]-tablero[r])==r-i or tablero[i]==tablero[r]:
-			return False
-	return True
+"""
+    @clase laboratorio2
+    Descripcion: Punto 2 del laboratorio 2
+    @author Mateo Ramirez H. / Juan Camilo Echeverri S.
+    @version 1
+"""
 
-
+#Metodo para generar una representacion del tablero con las restricciones
+#Y ejecutar las combinaciones posibles de reinas en el tablero con backtracking
 def n_reinas():
 	size=input("tamaño: ")
 	tablero=[]
@@ -19,8 +21,14 @@ def n_reinas():
 	tablero = [None]*int(size)
 	return len(n_reinas_aux([],0,tablero,restriccion)) 
 
+#Verificamos si es valido poner la reina en la posicion
+def puedo_poner_reina(r,tablero):
+	for i in range(r):
+		if abs(tablero[i]-tablero[r])==r-i or tablero[i]==tablero[r]:
+			return False
+	return True
 
-
+#Metodo para comprobar si la combinacion de reinas en el tablero es valida
 def n_reinas_aux(r,n,tablero,restriccion):
 	if n>=len(tablero):
 		r.append(tablero)
--- laboratorios/lab02/codigo/laboratorio2.py
+++ laboratorios/lab02/codigo/laboratorio2.py
@@ -1,4 +1,11 @@
-#punto 1.1
+"""
+    @clase laboratorio2
+    Descripcion: Punto 1.1 del laboratorio 2
+    @author Mateo Ramirez H. / Juan Camilo Echeverri S.
+    @version 1
+"""
+
+#Verificamos si es valido poner la reina en la posicion
 def es_valido(tablero):
 	for i in range(len(tablero)-1):
 		for k in range(i+1,len(tablero)):
@@ -6,6 +13,7 @@ def es_valido(tablero):
 				return False
 	return True
 
+#Metodo para comprobar si la combinacion de reinas en el tablero es valida
 def queens_aux(pre, pos, resultado):
 	if len(pos)==0:
 		resultado.append(pre)
@@ -13,6 +21,8 @@ def queens_aux(pre, pos, resultado):
 		queens_aux(pre+pos[i],pos[:i]+pos[i+1:],resultado)
 	return resultado
 
+#Metodo para  generar todas las posibles combinaciones de reinas en el tablero
+# y llevar un contador de las combinaciones validas
 def queens(n):
 	pos=[str(i) for i in range(n)]
 	permutaciones=queens_aux("",pos,[])
